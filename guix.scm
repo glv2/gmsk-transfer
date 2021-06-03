3b02d497663e4e474e45ad62b54c79af5c48dc87
Guillaume Le Vaillant <glv@posteo.net>
2021-06-04 00:03:55 +0200

    Add Guix build recipe

---
+++ guix.scm
@@ -0,0 +1,26 @@
+(use-modules (gnu packages autotools)
+             (gnu packages radio)
+             (guix build-system gnu)
+             ((guix licenses) #:prefix license:)
+             (guix packages))
+
+(define gmsk-transfer
+  (package
+    (name "gmsk-transfer")
+    (version "1.0.0")
+    (source #f)
+    (build-system gnu-build-system)
+    (native-inputs
+     `(("autoconf" ,autoconf)
+       ("automake" ,automake)))
+    (inputs
+     `(("hackrf" ,hackrf)
+       ("liquid-dsp" ,liquid-dsp)))
+    (synopsis "Program to transfer data by radio")
+    (description
+     "@code{gmsk-transfer} is a command-line program to send or receive data by
+radio using the GMSK modulation.")
+    (home-page #f)
+    (license license:gpl3+)))
+
+gmsk-transfer
