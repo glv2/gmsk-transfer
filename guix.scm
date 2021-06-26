c53019e10d1895d23511588b67cffbd916ee7346
Guillaume Le Vaillant <glv@posteo.net>
2021-06-26 13:31:46 +0200

    Update Guix build recipe

--- guix.scm
+++ guix.scm
@@ -1,6 +1,7 @@
 (use-modules (gnu packages autotools)
              (gnu packages radio)
              (guix build-system gnu)
+             (guix git-download)
              ((guix licenses) #:prefix license:)
              (guix packages))
 
@@ -8,7 +9,15 @@
   (package
     (name "gmsk-transfer")
     (version "1.0.0")
-    (source #f)
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/glv2/gmsk-transfer")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1w79i1gvr5f0wjwcwkh7kj4pw1758rxd1lzkchfk0q057cciv15m"))))
     (build-system gnu-build-system)
     (native-inputs
      `(("autoconf" ,autoconf)
@@ -20,7 +29,7 @@
     (description
      "@code{gmsk-transfer} is a command-line program to send or receive data by
 radio using the GMSK modulation.")
-    (home-page #f)
+    (home-page "https://github.com/glv2/gmsk-transfer")
     (license license:gpl3+)))
 
 gmsk-transfer
