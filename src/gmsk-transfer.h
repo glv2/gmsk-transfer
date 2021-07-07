066290320963ba31ef33392d83cd5c57d8c4ac13
Guillaume Le Vaillant <glv@posteo.net>
2021-07-07 14:32:04 +0200

    Add documentation for API functions

--- src/gmsk-transfer.h
+++ src/gmsk-transfer.h
@@ -23,8 +23,34 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 typedef struct gmsk_transfer_s *gmsk_transfer_t;
 
+/* Set the verbosity level
+ *  - v: if not 0, print some debug messages to stderr
+ */
 void gmsk_transfer_set_verbose(unsigned char v);
+
+/* Get the verbosity level */
 unsigned char gmsk_transfer_is_verbose();
+
+/* Initialize a new transfer
+ *  - radio_driver: radio to use (e.g. "io" or "driver=hackrf")
+ *  - emit: 1 for transmit mode; 0 for receive mode
+ *  - file: in transmit mode, read data from this file
+ *          in receive mode, write data to this file
+ *          if NULL, use stdin or stdout instead
+ *  - sample_rate: samples per second
+ *  - bit_rate: bits per second
+ *  - frequency: center frequency of the transfer in Hertz
+ *  - frequency_offset: set the frequency of the radio frequency_offset Hz
+ *    lower than the frequency of the transfer (can be negative to set the
+ *    frequency higher)
+ *  - gain: gain of the radio transceiver
+ *  - ppm: correction for the radio clock
+ *  - inner_fec: inner forward error correction code to use
+ *  - outer_fec: outer forward error correction code to use
+ *  - id: transfer id; when receiving, frames with a different id will be
+ *        ignored
+ *  - dump: if not NULL, write raw samples sent or received to this file
+ */
 gmsk_transfer_t gmsk_transfer_create(char *radio_driver,
                                      unsigned char emit,
                                      char *file,
@@ -38,11 +64,23 @@ gmsk_transfer_t gmsk_transfer_create(char *radio_driver,
                                      char *outer_fec,
                                      char *id,
                                      char *dump);
+
+/* Cleanup after a finished transfer */
 void gmsk_transfer_free(gmsk_transfer_t transfer);
+
+/* Start a transfer and return when finished */
 void gmsk_transfer_start(gmsk_transfer_t transfer);
+
+/* Interrupt a transfer */
 void gmsk_transfer_stop(gmsk_transfer_t transfer);
+
+/* Interrupt all transfers */
 void gmsk_transfer_stop_all();
+
+/* Print list of detected software defined radios */
 void gmsk_transfer_print_available_radios();
+
+/* Print list of supported forward error codes */
 void gmsk_transfer_print_available_forward_error_codes();
 
 #endif
